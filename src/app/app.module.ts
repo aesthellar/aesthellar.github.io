e2d973a09ca9ae7980f011eec1c92ed5717c62e6
aesthellar <m_dong@uncg.edu>
2019-10-16 16:32:42 -0400

    added styling and reference files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { ResumeComponent } from './resume/resume.component';
 import { WorkComponent } from './work/work.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    ContactComponent,
-    ProjectsComponent,
     ResumeComponent,
-    WorkComponent
+    WorkComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: AboutComponent },
+      { path: 'work', component: WorkComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'resume', component: ResumeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
